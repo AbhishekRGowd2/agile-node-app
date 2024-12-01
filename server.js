3fe99424017cffd2c5aac36bf68f7eb658c6b594
Abhishek R Gowda <abhiram201196@gmail.com>
2024-12-01 13:58:44 +0530

    Added /students endpoint

--- server.js
+++ server.js
@@ -6,4 +6,17 @@ app.get('/',(req,res)=>{
     res.status(200).json({"name":"agile-node-app"});
 })
 
+app.use('/students',(req,res)=>{
+res.status(200).json([
+    {
+        "name":"Abhi",
+        "age":28
+    },
+    {
+        "name":"Appu",
+        "age":26
+    }
+])
+})
+
 app.listen(3000);
